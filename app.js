35cbb486b8510587c5ec46c8ab34666cdf4661bf
Yuriy Lavrushin <yuriy.lavroushin@gmail.com>
2022-10-31 23:22:29 +0400

    refactor: rename class `editMode` to `item-editable` according to rule 3.1 in html-and-css.md

--- app.js
+++ app.js
@@ -85,11 +85,11 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("editMode");
-    //If class of the parent is .editmode
+    var containsClass=listItem.classList.contains("item-editable");
+    //If class of the parent is .item-editable
     if(containsClass){
 
-        //switch to .editmode
+        //switch to .item-editable
         //label becomes the inputs value.
         label.innerText=editInput.value;
         editBtn.innerText="Edit";
@@ -98,8 +98,8 @@ var editTask=function(){
         editBtn.innerText="Save";
     }
 
-    //toggle .editmode on the parent.
-    listItem.classList.toggle("editMode");
+    //toggle .item-editable on the parent.
+    listItem.classList.toggle("item-editable");
 };
 
 
